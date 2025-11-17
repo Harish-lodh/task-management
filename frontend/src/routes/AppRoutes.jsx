23ef59b72d9ac161b898312f99f10d24097cdb39
Harish-lodh <harishlodh444@gmail.com>
2025-11-17 12:24:02 +0530

    fix

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Routes, Route, Navigate } from "react-router-dom";
-import Login from "../pages/login";
+import Login from "../pages/Login";
 import Layout from "../layout/Layout";
 import Dashboard from '../pages/Dashboard';
 
