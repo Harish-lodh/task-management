99d93b7dbed3082a30a9ee4cb6e9a4dd17933c4b
Harish-lodh <harishlodh444@gmail.com>
2025-11-17 12:31:58 +0530

    fix routes

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -17,7 +17,7 @@ export default function Login() {
       localStorage.setItem("token", data.token);
       localStorage.setItem("user", JSON.stringify(data.user));
 
-      navigate("/tickets");
+      navigate("/dashboard");
     } catch (err) {
       alert("Invalid credentials");
     }
